f56739dac48bb7054d53c3443e323c40a36e9734
Eric Whitmire <ewhitmire91@gmail.com>
2014-05-04 17:11:01 -0400

    remove tls

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -37,7 +37,7 @@ if ON_OPENSHIFT:
     EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
     EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
+    EMAIL_USE_TLS = False
 else:
     from sendgrid_settings import *
 
